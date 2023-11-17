36a6a3399b2204cda278dca284764dd514258273
thanhphat113 <thanhphat9523@gmail.com>
2023-11-17 18:54:24 +0700

    hmm

--- DAO/hoadonbandao.cs
+++ DAO/hoadonbandao.cs
@@ -4,8 +4,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Doanqlchdt.DTO
 {
@@ -31,34 +33,46 @@ namespace Doanqlchdt.DTO
             {
                 String mhdb = reader.GetString(0);
                 String manv = reader.GetString(1);
-                String makh= reader.GetString(2);
+                String makh = reader.GetString(2);
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
-                DateTime ngaytao=reader.GetDateTime(5);
-                hoadonbandto hdbdto=new hoadonbandto(mhdb,manv,makh,makm,tongtien,ngaytao );
-                ds.Add(hdbdto);
+                DateTime ngaytao = reader.GetDateTime(5);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
             return ds;
         }
-        public int insert(hoadonbandto hdbdto)
+        public void insert(hoadonbandto hd)
         {
-            connect.connect cn = new connect.connect();
-            SqlCommand sqlcommand = new SqlCommand();
-            sqlcommand.CommandType = System.Data.CommandType.Text;
-            sqlcommand.CommandText = "insert into HoaDonBan values(N'" + hdbdto.Mhdb + "',N'" + hdbdto.Mnv + "',N'" + hdbdto.Mkh + "',N'" + hdbdto.Mkm + "','" + hdbdto.Tongtien + "','"+hdbdto.Ngaytao+"')";
-            SqlConnection connect = cn.connection();
-            int kq = sqlcommand.ExecuteNonQuery();
-            connect.Close();
-            return kq;
+            try
+            {
+                using (SqlConnection conn = new connectToan().connection())
+                {
+                    string query = "insert into HoaDonBan values(@maHD,@maNV,@maKH,@maKM,@Tong,@ngayTao)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@maHD", hd.Mhdb);
+                    cmd.Parameters.AddWithValue("@maNV", hd.Mnv);
+                    cmd.Parameters.AddWithValue("@maKH", hd.Mkh);
+                    cmd.Parameters.AddWithValue("@maKM", hd.Mkm);
+                    cmd.Parameters.AddWithValue("@Tong", hd.Tongtien);
+                    cmd.Parameters.AddWithValue("@ngayTao", hd.Ngaytao);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
-        public int update(hoadonbandto hdbdto)
+        public int update(hoadonbandto hd)
         {
             connect.connect cn = new connect.connect();
             SqlCommand sqlcommand = new SqlCommand();
             sqlcommand.CommandType = System.Data.CommandType.Text;
-            sqlcommand.CommandText = "update HoaDonBan set MaNV= N'" + hdbdto.Mnv + "',MaKH= N'" + hdbdto.Mkh + "' ,MaKM= N'" + hdbdto.Mkm + "',TongTien='" + hdbdto.Tongtien + "',NgayTao='"+hdbdto.Ngaytao+"' where MaHDB='" + hdbdto.Mhdb + "' ";
+            sqlcommand.CommandText = "update HoaDonBan set MaNV= N'" + hd.Mnv + "',MaKH= N'" + hd.Mkh + "' ,MaKM= N'" + hd.Mkm + "',TongTien='" + hd.Tongtien + "',NgayTao='" + hd.Ngaytao + "' where MaHDB='" + hd.Mhdb + "' ";
             SqlConnection connect = cn.connection();
             int kq = sqlcommand.ExecuteNonQuery();
             connect.Close();
@@ -71,7 +85,7 @@ namespace Doanqlchdt.DTO
             SqlConnection connect = cn.connection();
             SqlCommand sqlcommand = new SqlCommand();
             sqlcommand.CommandType = System.Data.CommandType.Text;
-            sqlcommand.CommandText = "select * from HoaDonBan MaHDB LIKE '"+ma+"%'";
+            sqlcommand.CommandText = "select * from HoaDonBan MaHDB LIKE '" + ma + "%'";
             sqlcommand.Connection = connect;
             SqlDataReader reader = sqlcommand.ExecuteReader();
             while (reader.Read())
@@ -82,8 +96,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
@@ -107,8 +121,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
@@ -132,8 +146,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
@@ -157,8 +171,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
@@ -182,8 +196,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
@@ -207,8 +221,8 @@ namespace Doanqlchdt.DTO
                 String makm = reader.GetString(3);
                 int tongtien = reader.GetInt32(4);
                 DateTime ngaytao = reader.GetDateTime(5);
-                hoadonbandto hdbdto = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
-                ds.Add(hdbdto);
+                hoadonbandto hd = new hoadonbandto(mhdb, manv, makh, makm, tongtien, ngaytao);
+                ds.Add(hd);
             }
             reader.Close();
             connect.Close();
